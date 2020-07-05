8fdbe0e472c93fc851e5e62174f4339f582db58b
zunda-pixel <zunda.pixel@gmail.com>
2020-07-05 21:16:06 +0900

    Initial Commit

---
+++ AnkoMovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnkoMovie
+//
+//  Created by zunda on 2020/07/05.
+//  Copyright © 2020 zunda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
